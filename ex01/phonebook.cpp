39735f679865f52f6d1aee717dc34d8e064f3dd3
gru <damienantoine57@gmail.com>
2024-11-05 16:00:46 +0100

    started Ex01

---
+++ ex01/phonebook.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+
+class Contact
+{
+private :
+	std::string first_name;
+	std::string last_name;
+	std::string nickname;
+	std::string phone_number;
+	std::string darkest_secret;
+
+public:
+	Contact() : first_name(""), last_name(""), nickname(""), phone_number(""), darkest_secret("") {}
+
+	void setContact(
+		const std::string &first_name,
+		const std::string &last_name,
+		const std::string &nickname,
+		const std::string &phone_number,
+		const std::string &darkest_secret
+	) {
+		this->first_name = first_name;
+		this->last_name = last_name;
+		this->nickname = nickname;
+		this->phone_number = phone_number;
+		this->darkest_secret = darkest_secret;
+	}
+
+	void displayContact() const
+	{
+		std::cout << "First Name: " << first_name
+		<< ", Last Name: " << last_name
+		<< ", Nickname: " << nickname
+		<< ", Phone Number: " << phone_number
+		<< std::endl;
+	}
+};
+
+class PhoneBook
+{
+private:
+	static const int	max_contacts = 8;
+	Contact				contacts[max_contacts];
+	int					contact_count;
+	int					next_index;
+
+public:
+	PhoneBook() : contact_count(0), next_index(0) {}
+
+	void	add_contact (
+	const std::string &first_name,
+	const std::string &last_name,
+	const std::string &nickname,
+	const std::string &phone_number,
+	const std::string &darkest_secret)
+	{
+		contacts[next_index].setContact(first_name, last_name, nickname, phone_number, darkest_secret);
+		next_index = (next_index + 1) % max_contacts;
+		if (contact_count < max_contacts)
+			contact_count++;
+		std::cout << "Contact added successfully." << std::endl;
+	}
+
+	void search_contacts() const
+	{
+		static int i;
+		if (contact_count == 0)
+			std::cout << "No contacts." << std::endl;
+		else
+		{
+			i = 0;
+			while (i < contact_count)
+			{
+				std::cout << "Contact " << i + 1 << ": ";
+				contacts[i].displayContact();
+				i++;
+			}
+		}
+	}
+};
+
+void	getInput(const std::string &prompt, std::string &input)
+{
+	while (input.empty())
+	{
+		std::cout << prompt;
+		std::getline(std::cin, input);
+		if (input.empty())
+			std::cout << "Input cannot be empty." << std::endl;
+	}
+}
+
+int	main()
+{
+	PhoneBook phonebook;
+	std::string command;
+
+	while (1)
+	{
+		std::cout << "Enter command." << std::endl;
+		std::cout << "(Available commands : ADD ; SEARCH ; EXIT)" << std::endl;
+		std::getline(std::cin, command);
+
+		if (command == "ADD" || command == "add")
+		{
+			std::string first_name, last_name, nickname, phone_number, darkest_secret;
+			getInput("Enter first name: ", first_name);
+            getInput("Enter last name: ", last_name);
+            getInput("Enter nickname: ", nickname);
+            getInput("Enter phone number: ", phone_number);
+            getInput("Enter darkest secret: ", darkest_secret);
+            phonebook.add_contact(first_name, last_name, nickname, phone_number, darkest_secret);
+            std::cout << std::endl;
+		}
+		else if (command == "SEARCH" || command == "search")
+		{
+			phonebook.search_contacts();
+			std::cout << std::endl;
+		}
+		else if (command == "EXIT" || command == "exit")
+			break ;
+		else
+		{
+			std::cout << "Invalid command." << std::endl;
+			std::cout << std::endl;
+		}
+	}
+}
